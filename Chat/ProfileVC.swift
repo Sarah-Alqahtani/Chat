dbc3a2f659d4d810a2bf527c78ba40ea4bacd6c6
Sarah-Alqahtani <91519771+Sarah-Alqahtani@users.noreply.github.com>
2022-01-06 14:13:36 +0000

    Section7

--- Chat/ProfileVC.swift
+++ Chat/ProfileVC.swift
@@ -13,23 +13,50 @@ class ProfileVC: UIViewController {
 
     @IBAction func logout(_ sender: UIButton) {
         
-        do {
-                    try FirebaseAuth.Auth.auth().signOut()
-                    let vc = self.storyboard?.instantiateViewController(withIdentifier: "SigninVC") as! SigninVC
-                    let nav = UINavigationController(rootViewController: vc)
-                    nav.modalPresentationStyle = .fullScreen
-                    self.present(nav, animated: false)
-                } catch {
-                    print("Error Log Out User: \(error.localizedDescription)")
-                }
+        let actionSheet = UIAlertController(title: "", message: "", preferredStyle: .actionSheet)
+        actionSheet.addAction(UIAlertAction(title: "Log Out", style: .destructive, handler:{[weak self] _ in
+            guard let strongSelf = self else {
+                return
+            }
+      
+            do {
+                        try FirebaseAuth.Auth.auth().signOut()
+                let vc = strongSelf.storyboard?.instantiateViewController(withIdentifier: "SigninVC") as! SigninVC
+                        let nav = UINavigationController(rootViewController: vc)
+                        nav.modalPresentationStyle = .fullScreen
+             
+            strongSelf.present(nav, animated: true)
+                
+                    } catch {
+                        print("Error Log Out User: \(error.localizedDescription)")
+                    }
+    }))
+    actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+    present(actionSheet , animated: true)
+   
+}
+
         
-    }
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
     
+//    func alertCancel(){
+//        let actionSheet = UIAlertController(title: "", message: "", preferredStyle: .actionSheet)
+//        actionSheet.addAction(UIAlertAction(title: "Log Out", style: .destructive, handler:{[weak self] _ in
+//            guard let strongSelf = self else {
+//                return
+//            }
+//
+//        }))
+//        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+//        present(actionSheet , animated: true)
+//
+//    }
+//
 
-   
 
 }
